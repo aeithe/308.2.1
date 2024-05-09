b7935f34f377cbc227aa74de5fa2418f25c2147f
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-09 16:38:29 -0400

    fixed code and started on prompt 2

--- app.js
+++ app.js
@@ -8,17 +8,36 @@ const startingPlantAmmount = 20;
 //console.log(totalAmountofPlants);
 
 //if # of plants are doubled each week then 2nd week then starting area
-//not big enough to support more than 2 weeks of growth  bc 3 weeks = 80
 //total # of plants cannot exceeed 98 
 
-const weekCoificient = 2; 
+const weekCoefficient = 2; 
 let weekNum = 0;
 
 function weeklyPlantCalc (weekNum){
-        console.log(startingPlantAmmount * weekCoificient **weekNum);
+        console.log(startingPlantAmmount * weekCoefficient **weekNum);
     }
 
-weeklyPlantCalc(1);
+
+//change number inside parenthesis to whatever week number you want to fix    
+weeklyPlantCalc(2);
+
+if (weekNum <3){
+    console.log("The plants all fit inside the starting area at this time")
+}
+else {
+    console.log("The plants will not fit inside the starting area at this time")
+}
+
+const pruned = totalAmountofPlants*.80;
+const planted = totalAmountofPlants*.50;
+
+console.log(pruned, planted);
+
+
+
+
+
+
 
 
 
