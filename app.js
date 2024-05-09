718f32363e78f1f6bb79c9b4107fd20d23829cda
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-09 15:05:33 -0400

    did first attempt at creating calculator

--- app.js
+++ app.js
@@ -3,5 +3,26 @@ const radius = 5;
 const area = PI * radius * radius;
 const plantSpace = 0.8; 
 const totalAmountofPlants = area / plantSpace;
+const startingPlantAmmount = 20; 
+//calculating total amount of plants possible in starting area
+//console.log(totalAmountofPlants);
 
-console.log(totalAmountofPlants);
+//if # of plants are doubled each week then 2nd week then starting area
+//not big enough to support more than 2 weeks of growth  bc 3 weeks = 80
+//total # of plants cannot exceeed 98 
+
+let weekNum = 1;
+const num1 = 20;
+function weeklyPlantCalc (weekNum, operator, num1){
+    if (operator === "*"){
+        let value = 1;
+        console.log(value);
+    }
+}
+
+weeklyPlantCalc(weekNum, num1, "*");
+
+
+
+
+
