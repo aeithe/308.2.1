3154f8de143b34937805bae8a5b7dec27d8a5171
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-09 14:28:22 -0400

    cacluated total amount of plants possible in area

---
+++ app.js
@@ -0,0 +1,7 @@
+const PI = 3.1415;
+const radius = 5;
+const area = PI * radius * radius;
+const plantSpace = 0.8; 
+const totalAmountofPlants = area / plantSpace;
+
+console.log(totalAmountofPlants);
