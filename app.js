df223635fb5a384ae3d27517d66798897bab7639
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-10 11:30:25 -0400

    added if else ifelse loop to code and fixed it up

--- app.js
+++ app.js
@@ -10,31 +10,46 @@ const startingPlantAmmount = 20;
 //if # of plants are doubled each week then 2nd week then starting area
 //total # of plants cannot exceeed 98 
 
-const weekCoefficient = 2; 
-let weekNum = 0;
+//const weekCoefficient = 2; 
+//let weekNum = 0;
 
 function weeklyPlantCalc (weekNum){
-        console.log(startingPlantAmmount * weekCoefficient **weekNum);
+      console.log();
+      const plantCount = startingPlantAmmount * Math.pow(2,weekNum);
+
+      if (plantCount > totalAmountofPlants * 0.8){
+        console.log(`Week ${weekNum}: The plants need to be pruned this week.`);        
+      } else if (plantCount >= totalAmountofPlants * 0.5 && 
+                 plantCount <= totalAmountofPlants * 0.8
+      )
+      {console.log(`Week ${weekNum}: The plants are growing at an acceptable rate.`);
     }
+        else{ console.log(`Week ${weekNum}: There is room in the garden for plants to be planted.`);}    
+    
+    }
+
+    weeklyPlantCalc(1); //Simulates growth for week #1
+    weeklyPlantCalc(2); //Simulates growth for week #2
+    weeklyPlantCalc(3); //Simulates growth for week #3
 
 
 //change number inside parenthesis to whatever week number you want to fix    
-weeklyPlantCalc(2);
+//weeklyPlantCalc(2);
 
-if (weekNum <3){
-    console.log("The plants all fit inside the starting area at this time and more can be planted")
-}
-if (weekNum=3){
-    console.log("The plants")
-}
-else {
-    console.log("The plants will not fit inside the starting area at this time")
-}
+//if (weekNum <3){
+   // console.log("The plants all fit inside the starting area at this time and more can be planted")
+//}
+//if (weekNum=3){
+    //console.log("The plants")
+//}
+//else {
+ //   console.log("The plants will not fit inside the starting area at this time")
+//}
 
-const pruned = totalAmountofPlants*.80;
-const planted = totalAmountofPlants*.50;
+//const pruned = totalAmountofPlants*.80;
+//const planted = totalAmountofPlants*.50;
 
-console.log(pruned, planted);
+//console.log(pruned, planted);
 
 
 
